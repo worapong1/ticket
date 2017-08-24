db07e5cd2c0f4199cc819ca1f74aedc518cdf520
worapong1 <31300205+worapong1@users.noreply.github.com>
2017-08-24 10:47:59 +0700

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php
+	$db_name = "emp01";
+	$mysql_username = "root";
+	$mysql_password = "";
+	$server_name = "localhost";
+	
+	$conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
+		
+?>
